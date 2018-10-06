aabc068cf8f5f4b54cbbf2c7a6dc30d4c4397fcf
Bhanu chander U <bhanuchander210@gmail.com>
2018-10-06 16:15:38 +0530

    Update alarm.py

--- 04.Data_Mining/nmstest/alarm.py
+++ 04.Data_Mining/nmstest/alarm.py
@@ -1,9 +1,13 @@
 import pandas as pd
 import matplotlib
 from matplotlib import pyplot as plt
-from scipy.stats import chisquare
 
-import numpy as np
+from mlxtend.preprocessing import TransactionEncoder
+from mlxtend.frequent_patterns import apriori, association_rules
+
+import warnings
+warnings.filterwarnings("ignore")
+
 
 # Configuring plot fonts
 
@@ -13,7 +17,11 @@ matplotlib.rc('font', **font)
 
 alarm = pd.read_csv('/home/bhanuchander/alarm.csv')
 
-names = ['OBJECTTYPE', 'LAYERRATE', 'TYPE', 'FUNCTYPE', 'PROBABLECAUSEQUALIFIER', 'NATIVEPROBABLECAUSE', 'SEVERITY']
+alarm['TIME'] = pd.to_datetime(alarm['TIME'], unit='ms')
+
+alarm['DAY'] = alarm['TIME'].dt.weekday_name
+
+names = ['OBJECTTYPE', 'PROBABLECAUSE', 'SEVERITY', 'DAY']
 
 alarm = alarm[names]
 
@@ -23,30 +31,54 @@ print 'Columns      : ', alarm.shape[1]
 
 # Always Fill na 'Unknown' for Nominal Data Set
 
-alarm.fillna('Unknown', inplace=True)
+# alarm['LAYERRATE'].fillna('LR_Unknown', inplace=True)
 
-# for n in names:
-#     grp = alarm[n].value_counts()
-#
-#     print grp
-#     grp.plot(kind ='bar')
-#     plt.title(n)
-#     plt.gcf().subplots_adjust(bottom=0.15)
-#     plt.show()
+for n in names:
 
-alarm = alarm.apply(lambda x : pd.factorize(x)[0])+1
+    print alarm[n].value_counts()
 
-print alarm.head()
 
-print alarm.values
 
-print pd.DataFrame([chisquare(alarm[x].values, f_exp=alarm.values.T, axis=1)[0] for x in alarm])
+dataset = []
+for i in range(0, alarm.shape[0]-1):
+    dataset.append([str(alarm.values[i,j]) for j in range(0, alarm.shape[1])])
+# dataset = alarm.to_xarray()
 
-print '---------------------------'
+oht = TransactionEncoder()
+oht_ary = oht.fit(dataset).transform(dataset)
+df = pd.DataFrame(oht_ary, columns=oht.columns_)
+print df.head()
 
-# df = pd.DataFrame({'a':['a','b','a'],'b':['f','e','e']})
-#
-# df = df.apply(lambda x : pd.factorize(x)[0])+1
-#
-# print pd.DataFrame([chisquare(df[x].values,f_exp=df.values.T,axis=1)[0] for x in df])
+print oht.columns_
 
+output = apriori(df, min_support=0.1, use_colnames=oht.columns_)
+
+print output
+
+config = [
+    ('antecedent support', 0.7),
+    ('support', 0.3),
+    # ('support', 0.3),
+    ('confidence', 0.95),
+    # ('conviction', 10)
+]
+
+for metric_type, th in config:
+    rules = association_rules(output, metric=metric_type, min_threshold=th)
+    if rules.empty:
+        print 'Empty Data Frame For Metric Type : ',metric_type,' on Threshold : ',th
+        continue
+    print rules.columns.values
+    print '----------------------------------------------'
+    print 'For the Metric : ', metric_type, ' Value : ', th
+    print '----------------------------------------------'
+    print rules[['antecedents', 'consequents', metric_type]]
+
+    support=rules.as_matrix(columns=['support'])
+    confidence=rules.as_matrix(columns=['confidence'])
+
+    plt.scatter(support, confidence, edgecolors='red')
+    plt.xlabel('support')
+    plt.ylabel('confidence')
+    plt.title(metric_type+' : '+str(th))
+    plt.show()
